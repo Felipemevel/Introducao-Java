78618e41b04b154543d694d852aeb8707110ba7a
Felipe <lipe.conta1632@gmail.com>
2026-02-11 17:14:31 -0300

    Adicionado a aula sobre switch cases

---
+++ JavaNinja/src/Condicoes/EstudoSwitchCases.java
@@ -0,0 +1,35 @@
+package Condicoes;
+
+import java.util.Scanner;
+
+public class EstudoSwitchCases {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Escolha um personagem: ");
+
+        System.out.println("1 - Naruto Uzumaki");
+        System.out.println("2 - Sasuke Uchiha");
+        System.out.println("3 - Sakura Haruno");
+
+        int escolha = scanner.nextInt();
+
+        switch (escolha){
+            case 1:
+                System.out.println("A escolha do usuário foi: Naruto Uzumaki");
+                break;
+            case 2:
+                System.out.println("A escolha do usuário foi: Sasuke Uchiha");
+                break;
+            case 3:
+                System.out.println("A escolha do usuário foi: Sakura Haruno");
+                break;
+            default:
+                System.out.println("Você não digitou uma resposta válida.");
+        }
+
+        scanner.close();
+
+    }
+}
