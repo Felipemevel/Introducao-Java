a5fb4977040efde9ff89bd643cc971dbd1143523
Felipe <lipe.conta1632@gmail.com>
2026-02-11 17:27:23 -0300

    Adicionado a aula sobre ternario

--- JavaNinja/src/Condicoes/ternarios.java
+++ JavaNinja/src/Condicoes/ternarios.java
@@ -1,4 +1,14 @@
 package Condicoes;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class ternarios {
+    public static void main(String[] args) {
+
+        short missoes = 1;
+
+        String nivel = (missoes >= 10) ? "Esse ninja está com mais de 10 missões" : "Esse ninja tem menos de 10 missões";
+
+        System.out.println(nivel);
+    }
 }
