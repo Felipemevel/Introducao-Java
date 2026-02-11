a586a99ea55195d3336832c2afe211bcd81fa846
Felipe <lipe.conta1632@gmail.com>
2026-02-11 15:59:47 -0300

    Introduzido condicionais em Java: If-Else

---
+++ JavaNinja/src/Condicoes/IfElse.java
@@ -0,0 +1,20 @@
+package Condicoes;
+
+public class IfElse {
+    public static void main(String[] args) {
+
+        String nome = "Naruto Uzumaki";
+        int idade = 17;
+        boolean hokage = false;
+        short missoes = 20;
+        String rank;
+
+        if (missoes == 10 && idade > 15) {
+            System.out.println("Rank: Chunnin");
+        } else if (missoes >= 20) {
+            System.out.println("Rank: Jounin");
+        }else {
+            System.out.println("Rank: Gennim");
+        }
+    }
+}
