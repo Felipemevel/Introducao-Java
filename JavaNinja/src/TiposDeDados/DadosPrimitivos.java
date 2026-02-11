e43e332f2aa19ee45199b18cc3707aeecf8d9f64
Felipe <lipe.conta1632@gmail.com>
2026-02-11 14:44:03 -0300

    Dados primitivos

---
+++ JavaNinja/src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,16 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args){
+        int idade = 16;
+        double altura = 1.64;
+        char inicial = 'N';
+        boolean vivo = true;
+        Long saldoBancario = 999999999L;
+        System.out.print(altura);
+        System.out.println(idade);
+        System.out.println(inicial);
+        System.out.println(vivo);
+        System.out.println(saldoBancario);
+    }
+}
