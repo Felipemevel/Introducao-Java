deb43ec2c3c34f749b0766558419a7209fb8e321
Felipe <lipe.conta1632@gmail.com>
2026-02-11 18:26:56 -0300

    Adicionado a aula sobre laços

---
+++ JavaNinja/src/Condicoes/LacosDeRepeticao.java
@@ -0,0 +1,20 @@
+package Condicoes;
+
+public class LacosDeRepeticao {
+    public static void main(String[] args) {
+
+        int numClones = 0;
+        int maxClones = 40;
+
+        /*
+        while (numClones <= maxClones) {
+            numClones++;
+            System.out.println("O Naruto fez o clone: " + numClones);
+        }
+        */
+
+        for (int i = 1; i <= maxClones; i++) {
+            System.out.println("O Naruto fez o clone: " + i);
+        }
+    }
+}
