5f41b943f8606bcaa64dbfbee682ea86e3f21763
Felipe <lipe.conta1632@gmail.com>
2026-02-11 16:56:15 -0300

    Adicionado a aula sobre Scanner

---
+++ JavaNinja/src/Condicoes/ScannerDoUsuario.java
@@ -0,0 +1,25 @@
+package Condicoes;
+
+import java.util.Scanner;
+
+public class ScannerDoUsuario {
+    public static void main(String[] args) {
+
+        Scanner caixaDeTexto = new Scanner(System.in);
+
+        System.out.println("Escolha o nome do ninja:");
+        String nome = caixaDeTexto.nextLine();
+        System.out.println("Nome do Ninja: " + nome);
+
+        System.out.println("Digite a idade do ninja:");
+        int idade = caixaDeTexto.nextInt();
+        if (idade >= 18){
+            System.out.println("Seu ninja tem " + idade + " anos, ele é maior de idade!");
+        } else {
+            System.out.println("Seu ninja tem " + idade + " anos, ele é menor de idade.");
+        }
+
+        caixaDeTexto.close();
+
+    }
+}
