554f92aac9762cc9d585ccf8183c4da5f355feca
Felipe <lipe.conta1632@gmail.com>
2026-02-16 20:49:33 -0300

    Adicionada aula de objetos de memoria

--- JavaNinja/src/NivelBasico/Arrays/Array.java
+++ JavaNinja/src/NivelBasico/Arrays/Array.java
@@ -3,13 +3,20 @@ package NivelBasico.Arrays;
 public class Array {
     public static void main(String[] args) {
 
-        String[] ninjas = new String[4];
+        String[] ninjas = new String[6];
 
         ninjas[0] = "Naruto Uzumaki";
         ninjas[1] = "Sasuke Uchiha";
         ninjas[2] = "Sakura Haruno";
         ninjas[3] = "Hinata Hyuga";
+        ninjas[4] = "Kakashi Hatake";
 
+        System.out.println(ninjas[5]);
+
+        int[] idade = new int[2];
+        idade[0] = 16;
+
+        System.out.println(idade[1]);
 
     }
 }
