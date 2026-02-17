20330a75303ca1469cef6f7663fab6648cdf584b
Felipe <lipe.conta1632@gmail.com>
2026-02-16 21:07:07 -0300

    Entendendo conceito inicial de garbage collettor

--- JavaNinja/src/NivelBasico/Arrays/Array.java
+++ JavaNinja/src/NivelBasico/Arrays/Array.java
@@ -11,12 +11,33 @@ public class Array {
         ninjas[3] = "Hinata Hyuga";
         ninjas[4] = "Kakashi Hatake";
 
-        System.out.println(ninjas[5]);
+        System.out.println("==================================");
+
+        System.out.println(ninjas[4]);
+
+        System.out.println("==================================");
 
         int[] idade = new int[2];
         idade[0] = 16;
 
         System.out.println(idade[1]);
+
+        System.out.println("==================================");
+
+        ninjas = new String[7];
+        ninjas[0] = "Hashirama Senju";
+        ninjas[1] = "Tobirama";
+        ninjas[2] = "Hiruzen Sarutobi";
+        ninjas[3] = "Minato Namikaze";
+        ninjas[4] = "Tsunada";
+        ninjas[5] = "Kakashi Hatake";
+        ninjas[6] = "Naruto Uzumaki";
+
+        for(int i = 0; i < ninjas.length; i++){
+            System.out.println(ninjas[i]);
+        }
+
+        System.out.println("==================================");
 
     }
 }
